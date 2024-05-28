be0febe2c3f9e92919f07716aae571bb9a5b1c1c
showgayaki <knk.lee.kj@gmail.com>
2024-05-29 00:24:23 +0900

    とりあえず実行環境構築

---
+++ akubi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace akubi
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        static void Main()
+        {
+            //var builder = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile(path: "appsettings.json");
+            //var configuration = builder.Build();
+
+            var service = new akubi();
+            // デバッグのときはコンソールアプリとして実行する
+            if (System.Environment.UserInteractive)
+            {
+                var eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, "StopService");
+                service.StartDebug(null);
+                eventWaitHandle.WaitOne();
+                service.StopDebug();
+            }
+            else
+            {
+                ServiceBase.Run(service);
+            }
+        }
+    }
+}
---
+++ akubi/akubi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akubi
+{
+    public partial class akubi : ServiceBase
+    {
+        public akubi()
+        {
+            InitializeComponent();
+        }
+        public void StartDebug(string[] args){ OnStart(args); }
+        public void StopDebug() { OnStop(); }
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
